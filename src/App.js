3d80e99c96cdf0cd97f968ac5a1eee61a5efce95
dkotula <dkotula.wieliczka@gmail.com>
2021-11-12 14:24:58 +0100

    Basic application

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Canvas from './Canvas'
+
+function App() {
+    let x, y;
+    const draw = (ctx, frameCount) => {
+        if (frameCount === 0 || frameCount % 100 === 0) {
+            x = 100 + Math.random() * 500;
+            y = 100 + Math.random() * 500;
+        }
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = "#000000";
+        ctx.beginPath();
+        ctx.arc(
+            x,
+            y,
+            20 * Math.sin(frameCount * 0.05) ** 2,
+            0,
+            2 * Math.PI * Math.sin(frameCount * 0.05) ** 2
+        );
+        ctx.fill();
+    };
+
+    return (
+        <Canvas draw={draw} width={window.innerWidth} height={window.innerHeight}/>
+    );
+}
+
+export default App
